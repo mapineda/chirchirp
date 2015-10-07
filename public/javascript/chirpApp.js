1ea61fe0b5cf6ce3bdf5da0122728c03b97e11af
mapineda Pineda <mapineda1222@gmail.com>
2015-10-07 15:16:43 -0500

    changes

--- public/javascript/chirpApp.js
+++ public/javascript/chirpApp.js
@@ -2,13 +2,13 @@
 var app = angular.module('chirApp', []);
 
 
-app.controller('mainController',function(){
-	$scope.posts = [];
+app.controller('mainController', function() {
+	$scope.posts = []
 	$scope.newPost = {created_by: '', text: '', created_at: ''};
 
 	$scope.post = function(){
 		$scope.newPost.created_at = Date.now();
-		$scope.post.push($scope.newPost);
+		$scope.posts.push($scope.newPost);
 		$scope.newPost = {created_by: '', text: '', created_at: ''};
 	};
 });
